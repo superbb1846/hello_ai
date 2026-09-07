80daaa84eedcecb7786b76bde6ecdd080c4eb0be
superbb1846 <superbb1846@gmail.com>
2026-09-07 08:31:44 +0000

    Add Counter contract with increment and add functions

--- firstconstract/demo_counter.sol
+++ firstconstract/demo_counter.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
 
+contract Counter {
+ uint public counter;
+ constructor() {
+ counter = 0;
+ }
+ function count() public {
+ counter = counter + 1;
+ }
+  function add(uint x) public {
+ counter = counter + x;
+ }
+}
